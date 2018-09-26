91d190a88d7f618f469b1abe38e6acec32df1229
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-26 09:33:21 -0400

    Final Program

--- Number Sorter/Program.cs
+++ Number Sorter/Program.cs
@@ -10,6 +10,24 @@ namespace Number_Sorter
     {
         static void Main(string[] args)
         {
+            //Write a program in C# Sharp to store elements in an array and print it.
+
+            var numbers = new[] {5, 7, 2, 1, 8, 3, 10};
+
+            int bse = 1;
+
+            for (int y = 0; y < 1000; y++)
+            {
+                foreach (var i in numbers)
+                {
+                    if (i == bse)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                bse++;
+            }
         }
     }
 }
